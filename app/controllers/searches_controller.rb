b760b1893ecbe37f5c2d967de594ded7cbe942a6
Jesse Wolgamott <jesse@comalproductions.com>
2016-07-21 11:56:59 -0500

    Add kaminari pagination with bootstrap

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
 
     if params[:qs].present?
       # actually do the search
-      @pages = Page.search(params[:qs])
+      @pages = Page.search(params[:qs]).page(params[:page])
       render :results
     else
       render :new

b2b4d64f9ab7e41726be27eb9821ac7e16ef78e6
Jesse Wolgamott <jesse@comalproductions.com>
2016-07-21 13:15:20 -0500

    Better search results

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,15 @@
 module SearchesHelper
+
+  def truncate_search(content, word)
+
+    index = content.index(word)
+    # Should return the meta description instead
+    return truncate(content, length: 400) unless index
+
+    start = index - 200
+    ends  = index + 200
+
+    content[start..ends]
+  end
+
 end
